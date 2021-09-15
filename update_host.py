0779a92cd496b00625fb89ce5f6feb55ac1e0ac4
mango <2015130324@email.szu.edu.cn>
2021-09-15 08:47:45 +0800

    add website list and update script

---
+++ update_host.py
@@ -0,0 +1,39 @@
+﻿import argparse
+import re
+
+def get_parser():
+    parser = argparse.ArgumentParser("srcipt for update host")
+    parser.add_argument("--ip", help="update ip")
+    parser.add_argument('--domain', help="domain list filename")
+    return parser
+
+def IsDomain(domain):
+    ds = re.findall('^(?=^.{3,255}$)[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+$', domain)
+    if(len(ds) != 0):
+        return True
+    else:
+        return False
+
+
+def update(ip, domain_file_path):
+    dns_list = ["#cloudflare host\n"]
+    with open(str(domain_file_path),'r') as fr:
+        line =fr.readline()
+        while line:
+            if (IsDomain(line)):
+                dns_list.append(ip + "\t\t" + line)
+            else:
+                dns_list.append(" \n")
+            line=fr.readline()
+    fr.close()
+    with open('host.txt', 'w') as fw:
+        for dns in dns_list:
+            fw.writelines(dns)
+    fw.close()
+
+if __name__ == '__main__':
+    parser = get_parser()
+    args = parser.parse_args()
+    ip = args.ip
+    domain_file = args.domain
+    update(ip, domain_file)
